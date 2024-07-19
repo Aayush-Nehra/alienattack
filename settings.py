23dc27f9a84b8d57ca4290f629074c87956072b5
Aayush Nehra <50041015+Aayush-Nehra@users.noreply.github.com>
2024-07-19 18:42:59 +0530

    move ship
    This code allows the ship to move left and right on the screen

---
+++ settings.py
@@ -0,0 +1,16 @@
+class Settings:
+    """A class to store all settings of alien invasion"""
+
+    def __init__(self):
+        """Initialize the game's settings."""
+        # Screen Settings
+        self.screen_width = 1200
+        self.screen_height = 800
+        self.bg_color = (230, 230, 230)
+        self.ship_speed = 1.5
+
+        # Bullet Settings
+        self.bullet_speed = 2
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = (60,60,60)
---
+++ ship.py
@@ -0,0 +1,39 @@
+import pygame
+class Ship:
+    """A class to manage ship"""
+
+    def __init__(self, ai_game):
+        """Initialise the ship and set its starting position"""
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+
+        # Load the ship image and get its rect
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+
+        #Movement flag; start with a ship that's not moving
+        self.moving_right = False
+        self.moving_left = False
+
+        # Start each new ship at the bottom center of the screen.
+        self.rect.midbottom = self.screen_rect.midbottom
+
+        # Store a float for sips exact horizontal position
+        self.x = float(self.rect.x)
+
+    def blitme(self):
+        """Draw the ship at its current location"""
+        self.screen.blit(self.image, self.rect)
+
+    def update(self):
+        """Update the ship's position based on the movement flag."""
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.x += self.settings.ship_speed
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+        # update rect obhect from self.x
+        self.rect.x = self.x
+        print( str(self.rect.left) + "  " + str(self.rect.x) + "  " + str(self.rect.right))
+        
