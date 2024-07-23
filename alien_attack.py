b01cbaef2df1d0801f3f2072fb7a8b5bbcc7c31d
Aayush-Nehra <iaayushnehra@gmail.com>
2024-07-23 18:18:46 +0530

    adding bullet firing

--- alien_attack.py
+++ alien_attack.py
@@ -2,6 +2,7 @@ import sys
 import pygame
 from settings import Settings
 from ship import Ship
+from bullet import Bullet
 
 
 class AlienAttack:
@@ -25,19 +26,15 @@ class AlienAttack:
         pygame.display.set_caption("Alien Invasion")
 
         self.ship = Ship(self)
+        self.bullets = pygame.sprite.Group()
 
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             self._check_events()
             self.ship.update()
+            self._update_bullet()
             self._update_screen()
-
-            self.screen.fill(self.settings.bg_color)
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible
-            pygame.display.flip()
             self.clock.tick(60)
 
     def _check_keydown_events(self, event):
@@ -48,6 +45,8 @@ class AlienAttack:
             self.ship.moving_left = True
         elif event.key == pygame.K_q:
             sys.exit()
+        elif event.key == pygame.K_SPACE:
+            self._fire_bullet()
 
     def _check_keyup_events(self, event):
         if event.key == pygame.K_RIGHT:
@@ -68,9 +67,23 @@ class AlienAttack:
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen"""
         self.screen.fill(self.settings.bg_color)
+        for bullet in self.bullets.sprites():
+             bullet.draw_bullet()
         self.ship.blitme()
-        #Make the most recently drawn screen visible
         pygame.display.flip()
+
+    def _fire_bullet(self):
+        """Create a new bullet and add it to bullets group"""
+        if len(self.bullets) < self.settings.bullets_allowed:
+            new_bullet = Bullet(self)
+            self.bullets.add(new_bullet)
+    
+    def _update_bullet(self):
+        """Update position of bullets and get rid of old bullets."""
+        self.bullets.update()
+        for bullet in self.bullets.copy():
+                if bullet.rect.bottom <= 0:
+                    self.bullets.remove(bullet)
 
 if __name__ == '__main__':
     #Make a game instance, and run the game
--- bullet.py
+++ bullet.py
@@ -1,20 +1,18 @@
 import pygame
-from pygame.sprite import _Group, Sprite
+from pygame.sprite import Group, Sprite
 
 class Bullet(Sprite):
     """A class to manage bullets fired form the ship"""
 
-    def __init__(self, ai_game):
+    def __init__(self, ai_game):   
         super().__init__()
         self.screen = ai_game.screen 
         self.settings = ai_game.settings
         self.color = self.settings.bullet_color
-
         # Create a bullet rect at (0, 0) and then set correct position
         self.rect = pygame.Rect(0,0,self.settings.bullet_width,
                                 self.settings.bullet_height)
         self.rect.midtop = ai_game.ship.rect.midtop
-
         # Store the bullet's position as a float
         self.y = float(self.rect.y)
 
