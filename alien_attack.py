fd937ad22c06d2ff67d6e179e1968a74738eae50
Aayush-Nehra <iaayushnehra@gmail.com>
2024-07-30 17:39:29 +0530

    creating fleet of aliens

--- alien_attack.py
+++ alien_attack.py
@@ -73,22 +73,28 @@ class AlienAttack:
             elif event.type == pygame.KEYUP:
                 self._check_keyup_events(event)
 
-    def _create_alien(self, x_position):
+    def _create_alien(self, x_position, y_position):
         """Create an alien and place it in a row"""
         new_alien = Alien(self)
         new_alien.x = x_position
         new_alien.rect.x = x_position
+        new_alien.rect.y = y_position
         self.aliens.add(new_alien)
 
     def _create_fleet(self):
         """Create a fleet of aliens"""
         alien = Alien(self)
-        alien_width = alien.rect.width
+        alien_width, alien_height = alien.rect.size
         
-        current_x = alien_width
-        while current_x < (self.settings.screen_width - 2 * alien_width):
-            self._create_alien(current_x)
-            current_x += 2 * alien_width
+        current_x, current_y = alien_width, alien_height
+        while current_y < (self.settings.screen_height - 3 * alien_height):
+            while current_x < (self.settings.screen_width - 2 * alien_width):
+                self._create_alien(current_x, current_y)
+                current_x += 2 * alien_width
+            
+            current_x = alien_width
+            current_y += 2 * alien_height
+
 
     def _update_screen(self):
         """Updates images on the screen, and flip to the new screen"""
